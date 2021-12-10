1702ae3338bb252551516f7cbd2411ac05b0ce33
Kirkland Childers <robert.kirkland.childers@gmail.com>
2021-12-10 13:27:30 -0500

    Implemented Purpose and added README

---
+++ README.md
@@ -0,0 +1,4 @@
+# HTML Table
+    ## Purpose
+        ### To send lists and dictionaries to front end using Python Flask. Secondary task was to implement animations to table rows from CSS Tricks for fun
+        ### https://css-tricks.com/making-css-animations-feel-natural/
--- server.py
+++ server.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+
+
 @app.route('/')
 def show_table():
-    return render_templates('index.html')
+    return render_template('index.html', ninjas= users)
 
 
 
